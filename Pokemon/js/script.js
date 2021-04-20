c11973dbdd857e0f47d66efd17c3261778b2995d
DamienLGRS <damien.legros@aio.eu>
2021-04-20 11:51:36 +0200

    choix de set

---
+++ Pokemon/js/script.js
@@ -0,0 +1,17 @@
+const API_URL = "https://api.pokemontcg.io/v2/"
+
+const form = document.getElementById("searchForm")
+
+fetch(`${API_URL}sets/`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        let setHTML = document.getElementById("selection-set")
+        
+        setHTML.innerHTML = `
+            <p>${data.name}</p>
+            <img src="${data.images}" alt=""/>
+        `
+        console.log(setHTML)
+    })
