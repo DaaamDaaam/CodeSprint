5e0913d7c3536f2e55e9b5a4a36d23f7b964cc88
DamienLGRS <damien.legros@aio.eu>
2021-04-21 17:37:19 +0200

    epaisseur + espacement prix

--- Pokemon/js/main.js
+++ Pokemon/js/main.js
@@ -14,27 +14,27 @@ submitHTML.addEventListener('click', function(event){
             let priceMin = ""
             if(pokemon.tcgplayer) {
               if(pokemon.tcgplayer.prices.holofoil){
-                priceMin = "low : " + pokemon.tcgplayer.prices.holofoil.low + "$"
+                priceMin = "<strong>low :</strong> " + pokemon.tcgplayer.prices.holofoil.low + "$"
               } else if(pokemon.tcgplayer.prices.stEditionHolofoil){
-                priceMin = "low : " + pokemon.tcgplayer.prices.stEditionHolofoil.low + "$"
+                priceMin = "<strong>low :</strong> " + pokemon.tcgplayer.prices.stEditionHolofoil.low + "$"
               }
             }
 
             let priceMoy = ""
             if(pokemon.tcgplayer) {
               if(pokemon.tcgplayer.prices.holofoil){
-                priceMoy = "moy : " + pokemon.tcgplayer.prices.holofoil.mid + "$"
+                priceMoy = "<strong>moy :</strong> " + pokemon.tcgplayer.prices.holofoil.mid + "$"
               } else if(pokemon.tcgplayer.prices.stEditionHolofoil){
-                priceMoy = "moy : " + pokemon.tcgplayer.prices.stEditionHolofoil.mid + "$"
+                priceMoy = "<strong>moy :</strong> " + pokemon.tcgplayer.prices.stEditionHolofoil.mid + "$"
               }
             }
 
             let priceMax = ""
             if(pokemon.tcgplayer) {
               if(pokemon.tcgplayer.prices.holofoil){
-                priceMax = "high : " + pokemon.tcgplayer.prices.holofoil.high + "$"
+                priceMax = "<strong>high :</strong> " + pokemon.tcgplayer.prices.holofoil.high + "$"
               } else if(pokemon.tcgplayer.prices.stEditionHolofoil){
-                priceMax = "high : " + pokemon.tcgplayer.prices.stEditionHolofoil.high + "$"
+                priceMax = "<strong>high :</strong> " + pokemon.tcgplayer.prices.stEditionHolofoil.high + "$"
               }
             }
 
@@ -43,9 +43,9 @@ submitHTML.addEventListener('click', function(event){
             pokemonCard.innerHTML = `
                 <img src="${pokemon.images.large}" class='pkmn-card'>
                 <div class="prix">
-                  <p>${priceMin}</p>
-                  <p>${priceMoy}</p>
-                  <p>${priceMax}</p>
+                  <p class="m-1">${priceMin}</p>
+                  <p class="m-1">${priceMoy}</p>
+                  <p class="m-1">${priceMax}</p>
                 </div>
             `
             cardContainerHTML.append(pokemonCard)
