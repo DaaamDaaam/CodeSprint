fb2b5a640ffcc12bbffdb3d1a4fc3afbab9ba1d7
DamienLGRS <damien.legros@aio.eu>
2021-04-22 16:50:09 +0200

    DarkMode + bar

---
+++ Pokemon/js/script.js
@@ -0,0 +1,10 @@
+function darkMode() {
+    var body = document.getElementById("body");
+    body.classList.toggle("dark-mode-body");
+  
+    var html = document.getElementById("html");
+    html.classList.toggle("dark-mode-html");
+  
+    var retour = document.getElementById("retour");
+    retour.classList.toggle("dark-mode-retour");
+  }
